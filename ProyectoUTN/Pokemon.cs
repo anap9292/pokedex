30dba6993302ca45b482d83d78affbf17ebbeae1
“anap9292” <anahiandrea.p@gmail.com>
2022-11-23 18:27:42 -0300

    Listo para subir

---
+++ ProyectoUTN/Pokemon.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoUTN
+{
+    public class Pokemon
+    {
+        //clase Base de Pokemons, estos son los datos que voy a leer de mi BD creando un objeto que voy a manipular en mi app
+        public int Numero { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+
+    }
+}
---
+++ ProyectoUTN/PokemonNegocio.cs
@@ -0,0 +1,8 @@
+﻿namespace ProyectoUTN
+{
+    public class PokemonNegocio
+    {
+        //En esta clase voy a crear los metodos de acceso a los datos de los pokemons
+
+    }
+}
